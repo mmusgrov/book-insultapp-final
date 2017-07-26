088edbe8f6117c255aacca87f307b5c0c97310b1
Michael Musgrove <mmusgrov@redhat.com>
2017-07-26 16:09:30 +0100

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,5 +1,4 @@
 package org.openshift;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,35 +6,29 @@ import java.sql.Statement;
 
 public class InsultGenerator {
 	public String generateInsult() {
-		String vowels = "AEIOU";
-		String article = "an";
-		String theInsult = "";
-		
+		String vowels = "AEIOU";  String article = "an";  String theInsult = "";
 		try {
-			String databaseURL = "jdbc:postgresql://";
+			String databaseURL = "jdbc:postgresql://"; 
 			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
 			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
-
 			String username = System.getenv("POSTGRESQL_USER");
 			String password = System.getenv("PGPASSWORD");
-			Connection connection = DriverManager.getConnection(databaseURL, username, password);
-
+			Connection connection = DriverManager.getConnection(databaseURL, username,   password);
 			if (connection != null) {
-				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun    from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
 				while (rs.next()) {
 					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
-					theInsult =  String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
-							rs.getString("second"), rs.getString("noun"));
+					theInsult =  String.format("Thou art %s %s %s %s!", article,     rs.getString("first"), rs.getString("second"), rs.getString("noun"));
 				}
 				rs.close();
 				connection.close();
 			}
 		} catch (Exception e) {
-			return "Database connection problem!";
+			return "Database connection problem!"; 
 		}
 		return theInsult;
 	}
